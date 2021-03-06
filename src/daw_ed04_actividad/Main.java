2a229adaadee8f8f8eebd8b5a464693c117cbea6
Segundo inicio <Segundo inicio@192.168.1.15>
2021-03-06 12:20:46 +0100

    Segundo commit. después de refactorizar

--- src/daw_ed04_actividad/Main.java
+++ src/daw_ed04_actividad/Main.java
@@ -9,21 +9,31 @@ package daw_ed04_actividad;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
-
-        miCuenta = new CCuenta("Pepe Fernández","1023-2464-90-1232256789",2500,0);
-        saldoActual = miCuenta.estado();
+        String nombre; 
+        
+        cuenta1 = new CCuenta("Pepe Fernández","1023-2464-90-1232256789",2500,0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es: " + saldoActual );
+        
+        opera(cuenta1);
+        
+        nombre=cuenta1.getNombre_cliente();
+        System.out.println ("El nombre del propietario de la cuenta es "+ nombre);
+    }
 
+    private static void opera(CCuenta cuenta1) {
         try {
-            miCuenta.retirar(2300);
+            float cantRetirar=2300;
+            cuenta1.retirar(cantRetirar);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            float cantIngresar=695;
+            cuenta1.ingresar(cantIngresar);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
